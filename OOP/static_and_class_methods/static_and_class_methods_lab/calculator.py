d52e992685400c1339cfc1316d41096850db8f1e
Kristiyan Radoslavov <radoslavov48@gmail.com>
2022-11-08 18:44:20 +0200

    new file

---
+++ OOP/static_and_class_methods/static_and_class_methods_lab/calculator.py
@@ -0,0 +1,34 @@
+class Calculator:
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        result = 1
+        for current_num in args:
+            result *= current_num
+
+        return result
+
+    @staticmethod
+    def divide(*args):
+        first_num = args[0]
+        for i in range(1, len(args)):
+            first_num /= args[i]
+
+        return first_num
+
+    @staticmethod
+    def subtract(*args):
+        first_num = args[0]
+        for i in range(1, len(args)):
+            first_num -= args[i]
+
+        return first_num
+
+
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
