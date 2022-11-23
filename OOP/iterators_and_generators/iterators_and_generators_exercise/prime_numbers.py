6aaeeb132f6e9dfab9b999b4fda21a2e76092e7e
Kristiyan Radoslavov <radoslavov48@gmail.com>
2022-11-23 17:42:04 +0200

    new file

---
+++ OOP/iterators_and_generators/iterators_and_generators_exercise/prime_numbers.py
@@ -0,0 +1,15 @@
+def get_primes(nums):
+
+    for current_num in nums:
+        counter = 0
+
+        for num in range(1, current_num + 1):
+            if current_num % num == 0:
+                counter += 1
+
+        if counter <= 2 and current_num > 1:
+            yield current_num
+
+
+
+print(list(get_primes([-2, 0, 0, 1, 1, 0])))
