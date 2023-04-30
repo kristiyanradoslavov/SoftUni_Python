df0332d859a0c18081635f07ef1419a96c193e5b
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-04-30 21:57:40 +0300

    commit new file

---
+++ project/pizza.py
@@ -0,0 +1,57 @@
+class Pizza:
+
+    def __init__(self, name, dough, max_number_of_toppings):
+        self.name = name
+        self.dough = dough
+        self.max_number_of_toppings = max_number_of_toppings
+        self.toppings = {}
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "":
+            raise ValueError("The name cannot be an empty string")
+        self.__name = value
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        if value == "":
+            raise ValueError("You should add dough to the pizza")
+        self.__dough = value
+
+    @property
+    def max_number_of_toppings(self):
+        return self.__max_number_of_toppings
+
+    @max_number_of_toppings.setter
+    def max_number_of_toppings(self, value):
+        if value <= 0:
+            raise ValueError("The maximum number of toppings cannot be less or equal to zero")
+
+        self.__max_number_of_toppings = value
+
+    def add_topping(self, topping):
+        dict_len = len(self.toppings)
+
+        if dict_len == self.__max_number_of_toppings:
+            raise ValueError("Not enough space for another topping")
+
+        if topping.topping_type in self.toppings:
+            self.toppings[topping.topping_type] += topping.weight
+        else:
+            self.toppings[topping.topping_type] = topping.weight
+
+    def calculate_total_weight(self):
+        total_weight = self.dough.weight
+
+        for current_topping in self.toppings.values():
+            total_weight += current_topping
+
+        return total_weight
