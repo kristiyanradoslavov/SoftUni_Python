771d3b3eb1453bdc919c2e96ff70fbc805d44959
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-05-01 23:48:25 +0300

    commit new file

--- Practice/photo_album.py
+++ Practice/photo_album.py
@@ -1,3 +1,6 @@
+import math
+
+
 class PhotoAlbum:
     MAX_PAGE_COUNT = 4
 
@@ -7,9 +10,37 @@ class PhotoAlbum:
 
     @classmethod
     def from_photos_count(cls, photos_count):
-        return cls(photos_count)
+        number_of_pages = math.ceil(photos_count / cls.MAX_PAGE_COUNT)
+
+        return cls(number_of_pages)
+
+    def add_photo(self, label):
+        for current_index, current_photo in enumerate(self.photos):
+            if len(current_photo) < self.MAX_PAGE_COUNT:
+                current_photo.append(label)
+                return f"{label} photo added successfully on page {current_index + 1} slot {len(current_photo)}"
+        else:
+            return "No more free slots"
+
+    def display(self):
+        result = []
+        for current_index, current_photo in enumerate(self.photos):
+            if current_index == 0:
+                result.append("-----------")
+
+            txt = "[] " * len(current_photo)
+            result.append(txt.rstrip())
+            result.append("-----------")
+
+        return "\n".join(result)
 
 
-photo = PhotoAlbum.from_photos_count(4)
-
-print(photo.photos)
+album = PhotoAlbum(2)
+# print(album.add_photo("baby"))
+# print(album.add_photo("first grade"))
+# print(album.add_photo("eight grade"))
+# print(album.add_photo("party with friends"))
+# print(album.photos)
+# print(album.add_photo("prom"))
+# print(album.add_photo("wedding"))
+print(album.display())
